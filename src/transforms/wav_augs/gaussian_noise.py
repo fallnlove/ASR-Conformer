a3dca0d040e12ef5f84861385ee410ac708c9878
Askar Tsyganov <ashtsyganov@edu.hse.ru>
2024-10-05 13:43:34 +0300

    Fix bug with gaussioan noise

--- src/transforms/wav_augs/gaussian_noise.py
+++ src/transforms/wav_augs/gaussian_noise.py
@@ -1,5 +1,6 @@
 from random import random
 
+import torch
 import torchaudio
 from torch import Tensor, distributions, nn
 
@@ -8,14 +9,14 @@ class GaussianNoise(nn.Module):
     def __init__(self, prob: float, std: float = 0.05, snr: float = 10):
         super().__init__()
         self.prob = prob
-        self.noise = distributions.Normal(0, std)
+        self.std = std
         self.snr = Tensor([snr])
 
     def __call__(self, data: Tensor):
+        noise = torch.empty(data.size)
+        noise.normal_(0, self.std)
         return (
-            torchaudio.functional.add_noise(
-                data, self.noise.sample(data.size), self.snr
-            )
+            torchaudio.functional.add_noise(data, noise, self.snr)
             if self.prob < random()
             else data
         )
